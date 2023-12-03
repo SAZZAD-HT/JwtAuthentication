39922ad3b43bae746d3c061b0ba549384aa0e267
SAZZAD-HT <sazzadaiub1@gmail.com>
2023-12-03 22:05:57 +0600

    Add project files.

---
+++ JwtAuthentication/Models/JwtAuthResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthentication.Models
+{
+    [Serializable]
+    public class JwtAuthResponse
+    {
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public long ExpiresInSeconds { get; set; }
+    }
+}
